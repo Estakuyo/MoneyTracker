625159d29b3058e90c43e7cccf2a0f6a36f2d027
DESKTOP-81V3C4K\LENOVO <eustaquiosalcedojohn@gmail.com>
2026-03-17 13:22:23 +0800

    Added layout expense page

--- frontend/src/pages/expenses.jsx
+++ frontend/src/pages/expenses.jsx
@@ -1,9 +1,16 @@
 import Card from "../components/card";
+import Button from "../components/button";
 
 const Expenses = () => {
   return (
-    <div className="main-wrapper">
-      <Card className="w-sm" title={"Expenses"} />
+    <div className="main-wrapper px-10 py-20 flex flex-col md:grid gap-5 md:py-10">
+      <Card className="w-full" title={"Total Expense"} />
+
+      <Card className="w-full" title={"Categories"} />
+
+      <Card className="w-full" title={"Expenses"} />
+
+      <Card className="w-full col-span-3" title={"Expense Chart"} />
     </div>
   );
 };
