5f07f79566d9e4e43745df072dc50e5ef7838597
EustaquioJM <eustaquiosalcedojohn@gmail.com>
2026-04-12 23:13:42 +0800

    Fix create table error sql

--- MoneyTracker.session.sql
+++ MoneyTracker.session.sql
@@ -2,37 +2,33 @@ CREATE TABLE `Users`(
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `email` VARCHAR(255) NOT NULL,
     `username` VARCHAR(255) NOT NULL,
-    `password` VARCHAR(255) NOT NULL,
-    `goals_id` INT NOT NULL
+    `password` VARCHAR(255) NOT NULL
 );
 CREATE TABLE `Categories`(
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `user_id` INT NOT NULL,
+    `user_id` INT UNSIGNED NOT NULL,
     `name` VARCHAR(255) NOT NULL,
-    `type` VARCHAR(255) NOT NULL
+    `type` VARCHAR(255) NOT NULL,
+    FOREIGN KEY (`user_id`) REFERENCES `Users`(`id`)
 );
 CREATE TABLE `Transactions`(
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `title` VARCHAR(255) NOT NULL,
     `price` INT NOT NULL,
     `date` DATE NOT NULL,
-    `category_id` INT NOT NULL,
-    `user_id` INT NOT NULL
+    `category_id` INT UNSIGNED NOT NULL,
+    `user_id` INT UNSIGNED NOT NULL,
+    FOREIGN KEY (`category_id`) REFERENCES `Categories`(`id`),
+    FOREIGN KEY (`user_id`) REFERENCES `Users`(`id`)
 );
 CREATE TABLE `Goals`(
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `user_id` INT UNSIGNED NOT NULL,
     `title` VARCHAR(255) NOT NULL,
     `amount` INT NOT NULL,
-    `status` BOOLEAN NOT NULL
+    `status` BOOLEAN NOT NULL,
+    FOREIGN KEY (`user_id`) REFERENCES `Users`(`id`)
 );
-ALTER TABLE `Users`
-ADD CONSTRAINT `users_id_foreign` FOREIGN KEY(`id`) REFERENCES `Categories`(`user_id`);
-ALTER TABLE `Goals`
-ADD CONSTRAINT `goals_id_foreign` FOREIGN KEY(`id`) REFERENCES `Users`(`goals_id`);
-ALTER TABLE `Transactions`
-ADD CONSTRAINT `transactions_category_id_foreign` FOREIGN KEY(`category_id`) REFERENCES `Categories`(`id`);
-ALTER TABLE `Transactions`
-ADD CONSTRAINT `transactions_user_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `Users`(`id`);
 ---@block
 SHOW TABLES;
 ---@block
@@ -42,7 +38,10 @@ SELECT *
 FROM categories;
 SELECT *
 FROM transactions;
+SELECT *
+FROM goals;
 ---@block
 DROP TABLE users,
 categories,
-transactions;
+transactions,
+goals;
