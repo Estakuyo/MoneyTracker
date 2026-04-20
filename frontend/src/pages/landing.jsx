3efcc35ea0c1e958385976352ed26331db84ab96
DESKTOP-81V3C4K\LENOVO <eustaquiosalcedojohn@gmail.com>
2026-04-20 13:03:09 +0800

    Refactored landing page hero section not finished

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,6 +1,7 @@
 import { BanknoteArrowUp } from "lucide-react";
 import { BanknoteArrowDown } from "lucide-react";
 import { SquareCheckBig } from "lucide-react";
+import { Dot } from "lucide-react";
 
 import Button from "../components/button";
 
@@ -8,36 +9,19 @@ const Landing = () => {
   return (
     <div className="flex flex-col justify-center items-center text-white">
       <div className="h-screen w-full flex flex-col justify-center items-center bg-linear-to-b from-primary-600 to-primary-800 px-4">
+        <div className="bg-white/25 backdrop-blur-xs rounded-4xl pr-2.5 mb-5">
+          <p className="text-xs font-semibold tracking-widest flex justify-center items-center">
+            <Dot size={30} color="gold" />
+            YOUR FINANCE TRACKER
+          </p>
+        </div>
         <h1 className="text-4xl text-center font-extrabold sm:text-7xl animate-slide-up">
           <span>Money</span>
           <span className="text-accent-500">Tracker</span>
         </h1>
-        <div className="flex flex-col gap-6 sm:gap-10 sm:flex-row mt-12 sm:mt-20 animate-slide-up-delay-1">
-          <div className="border-2 border-primary-200 p-5 rounded-4xl bg-primary-900">
-            <span className="flex gap-2.5 mb-2.5 text-2xl sm:text-3xl font-semibold">
-              <BanknoteArrowUp size={35} />
-              <h1>Earn</h1>
-            </span>
-            <p className="text-base sm:text-xl">Track your earnings</p>
-          </div>
-          <div className="border-2 border-primary-200 p-5 rounded-4xl bg-primary-900">
-            <span className="flex gap-2.5 mb-2.5 text-2xl sm:text-3xl font-semibold">
-              <BanknoteArrowDown size={35} />
-              <h1>Spend</h1>
-            </span>
-            <p className="text-base sm:text-xl">Track your expenses</p>
-          </div>
-          <div className="border-2 border-primary-200 p-5 rounded-4xl bg-primary-900">
-            <span className="flex gap-2.5 mb-2.5 text-2xl sm:text-3xl font-semibold">
-              <SquareCheckBig size={35} />
-              <h1>Achieve</h1>
-            </span>
-            <p className="text-base sm:text-xl">Achieve financial goals</p>
-          </div>
-        </div>
         <Button
           title={"Get Started"}
-          className="mt-12 sm:mt-20 p-4 sm:p-5 bg-primary-500 hover:bg-primary-600 text-xl sm:text-3xl rounded-4xl animate-slide-up-delay-2"
+          className="mt-10 p-4 sm:p-5 bg-primary-500 hover:bg-primary-600 text-xl sm:text-2xl rounded-4xl animate-slide-up-delay-2"
         />
       </div>
 
