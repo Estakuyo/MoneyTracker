d83d788ca64afad56bd8182fe39bf2abd85e57c5
Estakuyo <eustaquiosalcedojohn@gmail.com>
2026-04-26 00:15:58 +0800

    Added placeholder in home page frontend

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -13,83 +13,106 @@ import {
 // Components
 import Card from "../components/card";
 import Button from "../components/button";
+import Placeholder from "../components/placeholder";
 
 const Home = () => {
   const sampleDataChart = [
-    {
-      date: "11/06/2003",
-      expense: 200,
-      earnings: 600,
-    },
-    {
-      date: "11/07/2003",
-      expense: 100,
-      earnings: 300,
-    },
-    {
-      date: "11/08/2003",
-      expense: 900,
-      earnings: 1500,
-    },
-    {
-      date: "11/09/2003",
-      expense: 1150,
-      earnings: 150,
-    },
-    {
-      date: "11/10/2003",
-      expense: 1550,
-      earnings: 100,
-    },
-    {
-      date: "11/11/2003",
-      expense: 900,
-      earnings: 880,
-    },
-    {
-      date: "11/12/2003",
-      expense: 100,
-      earnings: 800,
-    },
+    // {
+    //   date: "11/06/2003",
+    //   expense: 200,
+    //   earnings: 600,
+    // },
+    // {
+    //   date: "11/07/2003",
+    //   expense: 100,
+    //   earnings: 300,
+    // },
+    // {
+    //   date: "11/08/2003",
+    //   expense: 900,
+    //   earnings: 1500,
+    // },
+    // {
+    //   date: "11/09/2003",
+    //   expense: 1150,
+    //   earnings: 150,
+    // },
+    // {
+    //   date: "11/10/2003",
+    //   expense: 1550,
+    //   earnings: 100,
+    // },
+    // {
+    //   date: "11/11/2003",
+    //   expense: 900,
+    //   earnings: 880,
+    // },
+    // {
+    //   date: "11/12/2003",
+    //   expense: 100,
+    //   earnings: 800,
+    // },
   ];
+
+  const sampleData = {
+    // expenses: {
+    //   total: "₱1,800",
+    //   thisMonth: "+₱500",
+    //   addedCount: 5,
+    // },
+    // earnings: {
+    //   total: "₱1,800",
+    //   thisMonth: "+₱500",
+    //   addedCount: 5,
+    // },
+    // savings: {
+    //   total: "₱1,800",
+    //   thisMonth: "+₱500",
+    //   addedCount: 5,
+    // },
+  };
 
   return (
     <div className="main-wrapper px-10 py-20 flex flex-col md:grid gap-5 md:py-10">
       <Card className="w-full col-span-3" title={"Overall Chart Report"}>
         <div className="h-96">
-          <ResponsiveContainer width="100%" height="100%">
-            <LineChart data={sampleDataChart}>
-              <CartesianGrid strokeDasharray={"3 3"} />
-              <YAxis width={"auto"} fontSize={"12px"} fontWeight={800} />
-              <XAxis
-                width={"auto"}
-                fontSize={"12px"}
-                fontWeight={800}
-                dataKey={"date"}
-              />
-              <Line
-                type="monotone"
-                dataKey={"expense"}
-                strokeWidth={3}
-                stroke="red"
-                dot={false}
-              />
-              <Line
-                type="monotone"
-                dataKey={"earnings"}
-                strokeWidth={3}
-                stroke="lightgreen"
-                dot={false}
-              />
-              <Legend
-                verticalAlign="top"
-                height={40}
-                wrapperStyle={{ fontSize: 14 }}
-                iconSize={12}
-              />
-              <Tooltip />
-            </LineChart>
-          </ResponsiveContainer>
+          {sampleDataChart.length > 0 ? (
+            <ResponsiveContainer width="100%" height="100%">
+              <LineChart data={sampleDataChart}>
+                <CartesianGrid strokeDasharray={"3 3"} />
+                <YAxis width={"auto"} fontSize={"12px"} fontWeight={800} />
+                <XAxis
+                  width={"auto"}
+                  fontSize={"12px"}
+                  fontWeight={800}
+                  dataKey={"date"}
+                />
+                <Line
+                  type="monotone"
+                  dataKey={"expense"}
+                  strokeWidth={3}
+                  stroke="red"
+                  dot={false}
+                />
+                <Line
+                  type="monotone"
+                  dataKey={"earnings"}
+                  strokeWidth={3}
+                  stroke="lightgreen"
+                  dot={false}
+                />
+                <Legend
+                  verticalAlign="top"
+                  height={40}
+                  wrapperStyle={{ fontSize: 14 }}
+                  iconSize={12}
+                />
+                <Tooltip />
+              </LineChart>
+            </ResponsiveContainer>
+          ) : (
+            <Placeholder />
+          )}
         </div>
       </Card>
 
@@ -103,22 +126,32 @@ const Home = () => {
           />
         }
       >
-        <div className="flex flex-col gap-6">
-          <h1 className="text-md font-semibold text-gray-500">
-            Total Overall Expenses
-          </h1>
-          <h1 className="text-5xl text-error-600 font-bold">₱1,800</h1>
-          <div className="flex justify-between">
-            <div className="flex flex-col">
-              <p className="font-bold text-gray-800">+₱500</p>
-              <p className="font-semibold text-gray-500">This Month</p>
-            </div>
-            <div className="flex gap-2 items-end">
-              <p className="font-bold text-gray-800">5</p>
-              <p className="font-semibold text-gray-500">Expenses Added</p>
+        {sampleData.expenses ? (
+          <div className="flex flex-col gap-6">
+            <h1 className="text-md font-semibold text-gray-500">
+              Total Overall Expenses
+            </h1>
+            <h1 className="text-5xl text-error-600 font-bold">
+              {sampleData.expenses.total}
+            </h1>
+            <div className="flex justify-between">
+              <div className="flex flex-col">
+                <p className="font-bold text-gray-800">
+                  {sampleData.expenses.thisMonth}
+                </p>
+                <p className="font-semibold text-gray-500">This Month</p>
+              </div>
+              <div className="flex gap-2 items-end">
+                <p className="font-bold text-gray-800">
+                  {sampleData.expenses.addedCount}
+                </p>
+                <p className="font-semibold text-gray-500">Expenses Added</p>
+              </div>
             </div>
           </div>
-        </div>
+        ) : (
+          <Placeholder />
+        )}
       </Card>
 
       <Card
@@ -131,22 +164,32 @@ const Home = () => {
           />
         }
       >
-        <div className="flex flex-col gap-6">
-          <h1 className="text-md font-semibold text-gray-500">
-            Total Overall Earnings
-          </h1>
-          <h1 className="text-5xl text-success-600 font-bold">₱1,800</h1>
-          <div className="flex justify-between">
-            <div className="flex flex-col">
-              <p className="font-bold text-gray-800">+₱500</p>
-              <p className="font-semibold text-gray-500">This Month</p>
-            </div>
-            <div className="flex gap-2 items-end">
-              <p className="font-bold text-gray-800">5</p>
-              <p className="font-semibold text-gray-500">Earnings Added</p>
+        {sampleData.earnings ? (
+          <div className="flex flex-col gap-6">
+            <h1 className="text-md font-semibold text-gray-500">
+              Total Overall Earnings
+            </h1>
+            <h1 className="text-5xl text-success-600 font-bold">
+              {sampleData.earnings.total}
+            </h1>
+            <div className="flex justify-between">
+              <div className="flex flex-col">
+                <p className="font-bold text-gray-800">
+                  {sampleData.earnings.thisMonth}
+                </p>
+                <p className="font-semibold text-gray-500">This Month</p>
+              </div>
+              <div className="flex gap-2 items-end">
+                <p className="font-bold text-gray-800">
+                  {sampleData.earnings.addedCount}
+                </p>
+                <p className="font-semibold text-gray-500">Earnings Added</p>
+              </div>
             </div>
           </div>
-        </div>
+        ) : (
+          <Placeholder />
+        )}
       </Card>
 
       <Card
@@ -159,22 +202,32 @@ const Home = () => {
           />
         }
       >
-        <div className="flex flex-col gap-6">
-          <h1 className="text-md font-semibold text-gray-500">
-            Total Overall Savings
-          </h1>
-          <h1 className="text-5xl text-success-600 font-bold">₱1,800</h1>
-          <div className="flex justify-between">
-            <div className="flex flex-col">
-              <p className="font-bold text-gray-800">+₱500</p>
-              <p className="font-semibold text-gray-500">This Month</p>
-            </div>
-            <div className="flex gap-2 items-end">
-              <p className="font-bold text-gray-800">5</p>
-              <p className="font-semibold text-gray-500">Savings Added</p>
+        {sampleData.savings ? (
+          <div className="flex flex-col gap-6">
+            <h1 className="text-md font-semibold text-gray-500">
+              Total Overall Savings
+            </h1>
+            <h1 className="text-5xl text-success-600 font-bold">
+              {sampleData.savings.total}
+            </h1>
+            <div className="flex justify-between">
+              <div className="flex flex-col">
+                <p className="font-bold text-gray-800">
+                  {sampleData.savings.thisMonth}
+                </p>
+                <p className="font-semibold text-gray-500">This Month</p>
+              </div>
+              <div className="flex gap-2 items-end">
+                <p className="font-bold text-gray-800">
+                  {sampleData.savings.addedCount}
+                </p>
+                <p className="font-semibold text-gray-500">Savings Added</p>
+              </div>
             </div>
           </div>
-        </div>
+        ) : (
+          <Placeholder />
+        )}
       </Card>
     </div>
   );
